c68242a85c0b9b6403ef97a196441b673940b7f3
RamdlanFaqih <ramfaqih@gmail.com>
2023-12-31 17:55:13 +0700

    set fireworks

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [newYearMessage, setNewYearMessage] = React.useState([
     "Waiting for 2024"
   ]);
+  const [showFireworks, setShowFireworks] = React.useState(false)
   const particleInitialization = async (engine) => {
     await loadFireworksPreset(engine);
   };
@@ -19,11 +20,17 @@ function App() {
     return remainingTime;
   }
 
+  const handleCountdownComplete = () => {
+    setNewYearMessage(["2023 Wrapped !!", "Semoga Tahun Ini Bisa Lebih Baik lagi", "Bismilah"]);
+    setShowFireworks(true); 
+  }
+  
+
   return (
     <>
       <Particles
         init={particleInitialization}
-        options={{ preset: "fireworks" }}
+        options={{ preset: "fireworks", autoPlay: showFireworks }}
       />
       <div className="flex flex-col justify-center items-center min-h-screen gap-4">
         <span className="text-white text-4xl font-bold z-50">
@@ -35,7 +42,7 @@ function App() {
           />
         </span>
         <div className="z-50 text-white font-bold text-2xl">
-          <Countdown date={Date.now() + timeLeft()} onComplete={() => setNewYearMessage(["2023 Wrapped !!", "Semoga Tahun Ini Bisa Lebih Baik lagi", "Bismilah"])}/>
+          <Countdown date={Date.now() + timeLeft()} onComplete={handleCountdownComplete}/>
         </div>
       </div>
     </>
